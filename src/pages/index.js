709db9ab1a52ed98388e9b4868a777f0cc53d15b
florian gaebler <floriangaebler87@gmail.com>
2020-06-03 13:03:53 +0200

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container>
+      <Header headerText="Hello world!" />
+      <Link to="/contact/">Contact</Link>
+      <p>send us a message</p>
+    </Container>
+  )
 }
